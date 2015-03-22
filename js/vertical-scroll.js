b58beca501c895b4348b2cdfbe29aff96ca67850
Robert Bu <darkfall3@gmail.com>
2015-03-22 06:09:46 -0400

    redesign the site

---
+++ js/vertical-scroll.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+jQuery(document).ready(function(){
+	
+	$('a[href*=#]').click(function() {
+		 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+ 		&& location.hostname == this.hostname) {
+   var $target = $(this.hash);
+   $target = $target.length && $target
+   || $('[name=' + this.hash.slice(1) +']');
+   if ($target.length) {
+  	var targetOffset = $target.offset().top;
+  	$('html,body')
+  	.animate({scrollTop: targetOffset}, 500);
+    return false;
+   }
+ }
+  });			
+});
